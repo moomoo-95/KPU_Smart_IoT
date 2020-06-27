a895dfe0627bdc5bb66eeba822eb9b374d8a6f96
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-06-27 21:04:07 +0900

    renual

---
+++ SHILDE/app/src/main/obj/local/arm64-v8a/objs/jnilibsvm/libsvm/svm-train.o.d
@@ -0,0 +1,217 @@
+./obj/local/arm64-v8a/objs/jnilibsvm/libsvm/svm-train.o: \
+  jni\libsvm\svm-train.cpp \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\stdio.h \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__config \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\stdlib.h \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\string.h \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\ctype.h \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\errno.h \
+  jni\libsvm\..\common.h \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\string \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\string_view \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__string \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\algorithm \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\initializer_list \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cstddef \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\type_traits \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cstring \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\utility \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__tuple \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cstdint \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\stdint.h \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\version \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__debug \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\iosfwd \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\wchar.h \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\memory \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\typeinfo \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\exception \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cstdlib \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\new \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\limits \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\iterator \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__functional_base \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\tuple \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\stdexcept \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cassert \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\atomic \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\functional \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\bit \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cstdio \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cwchar \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cwctype \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cctype \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\wctype.h \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\vector \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__bit_reference \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\climits \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\limits.h \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__split_buffer \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\list \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\sstream \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\ostream \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\ios \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__locale \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\mutex \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__mutex_base \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\chrono \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\ctime \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\ratio \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\system_error \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__errc \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cerrno \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__threading_support \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\locale.h \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\support\android\locale_bionic.h \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\support\xlocale\__posix_l_fallback.h \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\streambuf \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\locale \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cstdarg \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__bsd_locale_fallbacks.h \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\bitset \
+  C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\istream \
+  jni\libsvm\svm-train.h jni\libsvm\svm.h
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\stdio.h:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__config:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\stdlib.h:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\string.h:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\ctype.h:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\errno.h:
+
+jni\libsvm\..\common.h:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\string:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\string_view:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__string:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\algorithm:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\initializer_list:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cstddef:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\type_traits:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cstring:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\utility:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__tuple:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cstdint:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\stdint.h:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\version:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__debug:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\iosfwd:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\wchar.h:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\memory:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\typeinfo:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\exception:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cstdlib:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\new:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\limits:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\iterator:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__functional_base:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\tuple:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\stdexcept:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cassert:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\atomic:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\functional:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\bit:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cstdio:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cwchar:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cwctype:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cctype:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\wctype.h:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\vector:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__bit_reference:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\climits:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\limits.h:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__split_buffer:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\list:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\sstream:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\ostream:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\ios:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__locale:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\mutex:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__mutex_base:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\chrono:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\ctime:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\ratio:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\system_error:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__errc:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cerrno:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__threading_support:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\locale.h:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\support\android\locale_bionic.h:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\support\xlocale\__posix_l_fallback.h:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\streambuf:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\locale:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\cstdarg:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\__bsd_locale_fallbacks.h:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\bitset:
+
+C:\Users\gtws2\AppData\Local\Android\Sdk\ndk\21.1.6352462\build\\..\sources\cxx-stl\llvm-libc++\include\istream:
+
+jni\libsvm\svm-train.h:
+
+jni\libsvm\svm.h:
