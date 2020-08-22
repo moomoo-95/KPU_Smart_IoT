8ab3c67f4f424e06f74ad956625c0e6793de05a8
star7062004 <star7062004@naver.com>
2020-08-23 03:18:34 +0900

    .

--- Arduino/sketch_Vibration.ino
+++ Arduino/sketch_Vibration.ino
@@ -1,29 +1,24 @@
 //--- 진동센서 SW-420 Test ---//
  
-int ledPin =13;
+
 int vib =11;
  
 void setup(){
-  pinMode(ledPin, OUTPUT);
+  //pinMode(ledPin, OUTPUT);
   pinMode(vib, INPUT); //센서핀 입력
   Serial.begin(9600); //시리얼통신 설정 9600
   Serial.println("----------------------vibration demo------------------------");
 }
 void loop(){
-  long measurement =TP_init();
+  long measurement =pulseIn (vib, HIGH);
   delay(50);
   Serial.print("measurment = ");
   Serial.println(measurement);
   if (measurement > 1000){
-    digitalWrite(ledPin, HIGH);
+   // digitalWrite(ledPin, HIGH);
   }
   else{
-    digitalWrite(ledPin, LOW); 
+    //digitalWrite(ledPin, LOW); 
   }
 }
  
-long TP_init(){
-  delay(10);
-  long measurement=pulseIn (vib, HIGH);
-  return measurement;
-}
